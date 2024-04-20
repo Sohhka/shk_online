97b6a29e84ceaf3d2a1c900f8120c66da970eb76
sσннкα <40715791+Sohhka@users.noreply.github.com>
2024-04-20 13:26:14 +0200

    reupload shk_online (12/12/2023 - 12h43)
    reupload shk_online (12/12/2023 - 12h43)

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+files {
+    'index.html',
+    'style.css',
+    'img/background.png',
+    'img/infos.png',
+    'img/devlogs.png',
+    'img/images.png',
+    'img/load.gif',
+    'img/cursor.png',
+    'Loading.ogg',
+}
+
+loadscreen 'index.html'
